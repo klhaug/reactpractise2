f0e0170f08b67beaaca579a8b3884cfa1e76c991
klhaug <kristian@snougle.no>
2024-09-26 10:03:18 +0200

    starting to learn on my own

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function Header() {
+  return(
+    <>
+      <h1>ReactPractise</h1>
+    </>
   );
 }
 
-export default App;
+function Button() {
+  const [count, setCount] = useState(0);
+  
+  function handleClick() {
+    setCount(count + 1)
+  }
+
+
+  return (
+    <>
+      <button onClick={handleClick}>This is a Reactbutton with {count} clicks!</button>
+    </>
+  );
+}
+
+export default function App() {
+  return(
+    <>
+      <Header />
+      <Button />
+    </>
+  )
+}
